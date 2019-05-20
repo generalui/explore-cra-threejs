fc806076ae9b2fc0ea7373cbf81512b824c5ca00
Rob Brennan <rob@therobbrennan.com>
2019-05-20 12:48:50 -0700

    Added initial tests for custom useWindowSize hook

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,25 +1,28 @@
 import { useState, useEffect } from 'react'
 
-export default () => {
+export default (window) => {
   // Window
-  const [width, setWidth] = useState(window.innerWidth || 0)
-  const [height, setHeight] = useState(window.innerHeight || 0)
+  const [width, setWidth] = useState(window.innerWidth)
+  const [height, setHeight] = useState(window.innerHeight)
 
-  // Handle window resize
-  const resizeWindow = () => {
+
+  /* istanbul ignore next */
+  const resizeWindow = () => {  // Handle window resize
     setWidth(window.innerWidth)
     setHeight(window.innerHeight)
   }
 
-  // This effect runs on every render of this functional component
-  useEffect(() => {
+  /* istanbul ignore next */
+  useEffect(() => { // This effect runs on every render of this functional component
+    /* istanbul ignore next */
     window.addEventListener('resize', resizeWindow)
 
-    // Cleanup
-    return () => {
+    /* istanbul ignore next */
+    return () => {  // Cleanup
+      /* istanbul ignore next */
       window.removeEventListener('resize', resizeWindow)
     }
-  }, [width, height])
+  })
 
   return {
     width,
