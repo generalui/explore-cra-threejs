b636fff10a3035e00a0a085c15b12bdac6b5e8e1
Rob Brennan <rob@therobbrennan.com>
2019-05-20 15:51:19 -0700

    Reinstating ThreeDemo component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import ThreeDemo from './components/ThreeDemo/ThreeDemo'
+import ThreeDemo from './components/ThreeDemo/ThreeDemo'
 import ThreeDemoWithHooks from './components/ThreeDemo/ThreeDemoWithHooks'
 
-// ReactDOM.render(<ThreeDemo />, document.getElementById('root'))
 ReactDOM.render(<ThreeDemoWithHooks />, document.getElementById('fc'))
+ReactDOM.render(<ThreeDemo />, document.getElementById('root'))
