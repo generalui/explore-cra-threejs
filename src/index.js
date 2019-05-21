f1cd86e168ccf71e3a47fcd39b99f48f4bdf5e64
Rob Brennan <rob@therobbrennan.com>
2019-05-21 09:45:33 -0700

    Added App component for ThreeDemoWithHooksV2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ThreeDemo from './components/ThreeDemo/ThreeDemo'
 import ThreeDemoWithHooks from './components/ThreeDemo/ThreeDemoWithHooks'
+import ThreeDemoWithHooksV2 from './threejs/components/App/App'
 
 ReactDOM.render(<ThreeDemoWithHooks />, document.getElementById('three-demo-with-hooks-v1'))
-// ReactDOM.render(<ThreeDemoWithHooks />, document.getElementById('three-demo-with-hooks-v2'))
+ReactDOM.render(<ThreeDemoWithHooksV2 />, document.getElementById('three-demo-with-hooks-v2'))
 ReactDOM.render(<ThreeDemo />, document.getElementById('three-demo-original'))
