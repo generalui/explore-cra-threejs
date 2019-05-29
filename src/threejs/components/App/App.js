20ac5a335fda5bb27d5bd9b24f25eb634435c50a
Johnny5 <johnny5@generalui.com>
2019-05-29 11:45:43 -0700

    Temporarily commenting out GLTF example

--- src/threejs/components/App/App.js
+++ src/threejs/components/App/App.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import CubeExample from '../../examples/cube/CubeExample'
-import GLTFExample from '../../examples/load-gltf/GLTFExample'
+// import GLTFExample from '../../examples/load-gltf/GLTFExample'
 
 export default () => {
   return (
     <Router>
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <div>
-            <h2>CRA three.js examples</h2>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <Route
+          exact
+          path="/"
+          render={() => (
             <div>
-              <Link to="/cube">Cube</Link>
+              <h2>CRA three.js examples</h2>
+              <div>
+                <Link to="/cube">Cube</Link>
+              </div>
+              <div>
+                <Link to="/gltf">GLTF 3D model</Link>
+              </div>
             </div>
-            <div>
-              <Link to="/gltf">GLTF 3D model</Link>
-            </div>
-          </div>
-        )}
-      />
-      <Route path="/cube" component={CubeExample} />
-      <Route path="/gltf" component={GLTFExample} />
-    </div>
-  </Router>
+          )}
+        />
+        <Route path="/cube" component={CubeExample} />
+        {/* <Route path="/gltf" component={GLTFExample} /> */}
+      </div>
+    </Router>
   )
 }
